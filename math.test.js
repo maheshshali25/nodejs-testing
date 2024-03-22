758a04bd061a5563fbe39b42cdcd5051320e5687
maheshshali25 <maiya9986@gmail.com>
2024-03-22 12:28:14 +0530

    first

---
+++ math.test.js
@@ -0,0 +1,19 @@
+const { add, subtract, divide, multiply } = require("./math");
+
+describe("Math.js tests", () =>{
+    it("should test add ", () =>{
+        expect(add(2, 2)).toBe(4);
+    });
+
+    it("should test subtract", () =>{
+        expect(subtract(2, 2)).toBe(0);
+    });
+
+    it("should test divide", () =>{
+        expect(divide(2, 2)).toBe(1);
+    });
+
+    it("should test multiply", ()=>{
+        expect(multiply(2, 2)).toBe(4);
+    });
+});
